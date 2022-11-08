f0be6a997004f450ac6df3a95b8d4ac73e91f6b0
elimarmacena <elimar.macena@gmail.com>
2022-11-07 22:04:15 -0300

    inserted git ignore
    also was performed some dependecy changes

--- graphql-start/src/main/java/dev/elimar/graphqlstart/GraphqlStartApplication.java
+++ graphql-start/src/main/java/dev/elimar/graphqlstart/GraphqlStartApplication.java
@@ -3,7 +3,7 @@ package dev.elimar.graphqlstart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class GraphqlStartApplication {
 
 	public static void main(String[] args) {
