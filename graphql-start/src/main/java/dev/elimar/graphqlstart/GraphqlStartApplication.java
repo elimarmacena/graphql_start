105a927787386cb786044ae2bce2f5a52bb1f8e9
elimarmacena <elimar.macena@gmail.com>
2022-11-07 21:21:31 -0300

    ADD: createad base project

---
+++ graphql-start/src/main/java/dev/elimar/graphqlstart/GraphqlStartApplication.java
@@ -0,0 +1,13 @@
+package dev.elimar.graphqlstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlStartApplication.class, args);
+	}
+
+}
---
+++ graphql-start/src/test/java/dev/elimar/graphqlstart/GraphqlStartApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elimar.graphqlstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
